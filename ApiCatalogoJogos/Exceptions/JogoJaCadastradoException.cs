20c5f9d702373fac1e5ed8704a59016fd5c868da
tonybusma <tonybusma@gmail.com>
2022-01-06 22:42:40 -0300

    API Catalogo de Jogos

---
+++ ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+
+    }
+}
